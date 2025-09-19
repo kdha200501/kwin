38993fa18c18126d14178289a165fdcbe6a33ec4
Jacks Diao <jacks.diao@agilysys.com>
2025-09-19 09:13:39 -0400

    jacks-customizations: bring back hide

--- src/useractions.cpp
+++ src/useractions.cpp
@@ -836,6 +836,8 @@ void Workspace::initShortcuts()
                  Qt::META | Qt::Key_Backspace, &Workspace::slotWindowRestore, true);
     initShortcut("Window Minimize", i18n("Minimize Window"),
                  Qt::META | Qt::Key_PageDown, &Workspace::slotWindowMinimize, false);
+    initShortcut("Window Minimize Others", "Minimize Other Windows",
+                 Qt::ALT | Qt::CTRL | Qt::Key_H, &Workspace::slotWindowMinimizeOthers, false);
     initShortcut("Window Shade", i18n("Shade Window"),
                  Qt::CTRL | Qt::Key_M, &Workspace::slotWindowShade, false);
     initShortcut("Window Move", i18n("Move Window"),
@@ -1126,6 +1128,32 @@ void Workspace::performWindowOperation(Window *window, Options::WindowOperation 
     case Options::MinimizeOp:
         window->setMinimized(true);
         break;
+    case Options::MinimizeOthersOp: {
+        if (
+            static_cast<NET::WindowType>(window->windowType()) == NET::Desktop ||
+            window->transientFor()
+        ) {
+            break;
+        }
+
+        QList<Window *> stackingOrder = workspace()->stackingOrder();
+        for (KWin::Window *client : stackingOrder) {
+            if (client == window) {
+                continue;
+            }
+
+            if (Window::belongToSameApplication(client, window, Window::SameApplicationCheck::AllowCrossProcesses)) {
+                continue;
+            }
+
+            if (client->windowType() != WindowType::Normal) {
+                continue;
+            }
+
+            client->setMinimized(true);
+        }
+        break;
+    }
     case Options::ShadeOp:
         // leach off the maximization mechanism to perform shading when the "Ctrl + M" combo is pressed
         window->maximize(window->maximizeMode() == MaximizeShade
@@ -1330,6 +1358,23 @@ void Workspace::slotWindowMinimize()
 {
     if (USABLE_ACTIVE_WINDOW) {
         performWindowOperation(m_activeWindow, Options::MinimizeOp);
+    }
+}
+
+void Workspace::slotWindowMinimizeOthers()
+{
+    if (USABLE_ACTIVE_WINDOW) {
+        performWindowOperation(m_activeWindow, Options::MinimizeOthersOp);
+    }
+}
+
+/**
+ * Shades/unshades the active window respectively.
+ */
+void Workspace::slotWindowShade()
+{
+    if (USABLE_ACTIVE_WINDOW) {
+        performWindowOperation(m_activeWindow, Options::ShadeOp);
     }
 }
 
