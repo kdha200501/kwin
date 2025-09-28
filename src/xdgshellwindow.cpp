a30d5c81ca4d8434deb8c4f580b49739992aa006
Jacks Diao <jacks.diao@agilysys.com>
2025-09-28 16:44:41 -0400

    jacks-customizations: fix Google Chrome Canary icon

--- src/xdgshellwindow.cpp
+++ src/xdgshellwindow.cpp
@@ -1525,6 +1525,11 @@ void XdgToplevelWindow::updateCapabilities()
 void XdgToplevelWindow::updateIcon()
 {
     if (!m_shellSurface->customIcon().isNull()) {
+        if(m_shellSurface->customIcon().name().toStdString() == "" && iconFromDesktopFile().toStdString() == "google-chrome-canary") {
+            setIcon(QIcon::fromTheme(QStringLiteral("google-chrome-canary")));
+            return;
+        }
+
         setIcon(m_shellSurface->customIcon());
         return;
     }
